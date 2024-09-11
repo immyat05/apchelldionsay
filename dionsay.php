eb73e1d4f2e134ff6b36a8722537b39b4d2d9eb4
immyat05 <dionsayapchell@gmail.com>
2024-09-11 12:26:15 +0800

    first commit

---
+++ dionsay.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+    echo "<h2> My Personal Information </h2>";
+    $Fullname="Apchell Mae D. Dionsay";
+    $Address="P-1,Yahong Tudela,Ozamiz City, Misamis Occidental";
+    $Contact="09473256894";
+    $Hobbies="Listen Music,Reading Novel, Watching Kdrama";
+    $Social_media="Facebook,Instagram,Tiktok";
+    
+    
+    echo "My name is: $Fullname <br>";
+    echo "I live in: $Address<br>";
+    echo "Contact: $Contact <br>";
+    echo "I love to: $Hobbies <br>";
+    echo "My social media account is:$Social_media <br>";
+
+    ?>
+    </div>
+</body>
+</html>
